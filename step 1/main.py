1f8eff12f3504822065849b6de119cf97be52210
Pranav Addepalli <pranavaddepalli@gmail.com>
2020-10-06 16:20:07 -0400

    added first step

---
+++ step 1/main.py
@@ -0,0 +1,9 @@
+import flask
+from flask import request, jsonify
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/', methods=['GET'])
+def home():
+    return "Hello, world!"
